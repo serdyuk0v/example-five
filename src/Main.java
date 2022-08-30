2ed0717015f218fca1488a2079717bd204128682
Serdyukov <and.serdykov@gmail.com>
2022-08-30 20:35:10 +0300

    Задача №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Целлочисленный массив
+        int [] arrayInt = new int[3];
+        arrayInt[0] = 1;
+        arrayInt[1] = 2;
+        arrayInt[2] = 3;
+
+        //Массив с дробными числами
+        double[] arrDouble = {1.57, 7.654, 9.986};
+        for (int i = 0; i < 3; i++) {
+            System.out.println(arrDouble[i]);
+        }
+
+        //Произвольный массив
+        boolean [] arrBoolean = new boolean[3];
+        arrBoolean[0] = arrDouble[1] >= arrayInt[2];
+        arrBoolean[1] = arrDouble[0] != arrayInt[1];
+        arrBoolean[2] = arrDouble[2] <= arrayInt[0];
+        System.out.println(arrBoolean[0] + " " + arrBoolean[1] + " " + arrBoolean[2]);
     }
 }
